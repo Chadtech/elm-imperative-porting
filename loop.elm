864c724771297fd06e8803f08372fa4b6a599271
Chadtech <chadstearns1@gmail.com>
2017-03-05 02:20:45 -0500

    documentation

--- loop.elm
+++ loop.elm
@@ -5,13 +5,32 @@ module Loop
         )
 
 
-{-| 
+{-| Just some loop functions.
 
 @docs while, until 
 
 -}
 
-{-| -}
+{-| The classic while loop. So classic. 
+
+
+    Loop.while ((>) 100) 0 ((+) 1)
+
+    -- is basically analagous to..
+    --
+    -- var i = 0;
+    -- while ( i < 100) {
+    --   i++;
+    -- }
+
+    ponder : String -> String
+    ponder this =
+        Loop.while 
+            (always False)
+            this
+            identity
+
+-}
 while : (a -> Bool) -> a -> (a -> a) -> a
 while condition state f =
     if condition state then
@@ -19,7 +38,17 @@ while condition state f =
     else 
         state
 
-{-| -}
+{-| A twist on the classic while loop. It loops until the condition is NOT met.
+
+
+    countJustForFun : Int -> Int
+    countJustForFun to =
+        Loop.until
+            ((==) to)
+            0
+            ((+) 1)
+
+-}
 until : (a -> Bool) -> a -> (a -> a) -> a
 until condition state f =
     if condition state then
