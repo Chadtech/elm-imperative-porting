de70b5e25cb0b93cb95d30c2e2e7051fa6b2ce43
Chadtech <chadstearns1@gmail.com>
2017-03-05 01:30:59 -0500

    loop dot elm

---
+++ loop.elm
@@ -0,0 +1,21 @@
+module Loop 
+    exposing 
+        ( while
+        , until
+        )
+
+
+while : (a -> Bool) -> a -> (a -> a) -> a
+while condition state f =
+    if condition state then
+        while condition (f state) f
+    else 
+        state
+
+
+until : (a -> Bool) -> a -> (a -> a) -> a
+until condition state f =
+    if condition state then
+        state
+    else 
+        until condition (f state) f
